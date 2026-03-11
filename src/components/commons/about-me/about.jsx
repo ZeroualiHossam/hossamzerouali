de64a6737ca0bd38c37ff685499b1311271951ac
Hossam Zerouali <hossam.zerouali@estudiantat.upc.edu>
2026-03-11 21:58:21 +0100

    Update About Me description

--- src/components/commons/about-me/about.jsx
+++ src/components/commons/about-me/about.jsx
@@ -5,7 +5,7 @@ const About = () => {
 return (
     <div className="container-about">
         <h1 className='intro'>Hi, I'm Hossam. Nice to meet you</h1>
-        <p className='descript'> I’m a Software Engineering student from Barcelona.
+        <p className='descript'> I’m a Software Engineering from Barcelona.
             Currently in my final year of college, I’m eager to gain more experience and hone my skills. 
             I’m a fast learner and a good problem solver. I enjoy taking on new challenges,
             leading tasks, and staying well-organized.</p>
